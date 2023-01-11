23f5ce809b5db0471f84a96d1b0620fa3300adac
RaulCruzT <cruztorresraul@gmail.com>
2023-01-10 22:10:59 -0300

    Fix Header And Awards

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,21 @@ import './navbar.css';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='top-food__navbar section__padding'>
+            <div className='top-food__navbar-container'>
+                <div className='top-food__navbar-container__logo'>
+                    <img src="../../../public/IoRestaurant.svg" alt="Logo" />
+                    <div className="top-food__navbar-container__logo-text">Top Food</div>
+                </div>
+                <div className='top-food__navbar-container__links'>
+                    <p><a href="#">Awards</a></p>
+                    <p><a href="#">Menu</a></p>
+                    <p><a href="#">Team</a></p>
+                    <p><a href="#">Enjoy</a></p>
+                    <p><a href="#">Contact</a></p>
+                </div>
+            </div>
+        </div>
     )
 }
 
