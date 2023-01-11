9847698a16b3960e89c6dd7e3fa6c60d9581ae33
RaulCruzT <cruztorresraul@gmail.com>
2023-01-11 16:15:00 -0300

    Add Mobile Navbar Style

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import { Links, Logo } from '../../components';
+import { IoMenuSharp, IoCloseSharp } from 'react-icons/io5';
 import './navbar.css';
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
         <div className='top-food__navbar section__padding'>
             <div className='top-food__navbar-container'>
@@ -9,6 +13,17 @@ const Navbar = () => {
                 <div className='top-food__navbar-container__links'>
                     <Links />
                 </div>
+                <div className="top-food__navbar-container__menu">
+                    {toggleMenu
+                        ? <IoCloseSharp color="#000000" size={28} onClick={() => setToggleMenu(false)} />
+                        : <IoMenuSharp color="#000000" size={28} onClick={() => setToggleMenu(true)} />
+                    }
+                </div>
+            </div>
+            <div className="top-food__navbar-menu" style={{height: toggleMenu ? '100vh': 0}}>
+                {toggleMenu &&
+                    <Links />
+                }
             </div>
         </div>
     )
